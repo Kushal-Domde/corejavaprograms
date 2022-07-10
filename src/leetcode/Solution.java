1f54f25936aeed2a9ee7f4803a13530b5f96e2ba
kaustubh23dom <kaustubh23dom>
2022-07-11 01:15:37 +0530

    first commit

---
+++ src/leetcode/Solution.java
@@ -0,0 +1,33 @@
+// Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+
+package leetcode;
+
+public class Solution 
+{
+	
+	 public static int countOdds(int low, int high) 
+	{
+	    
+	            if(low%2==0 && high%2==0)
+	            {
+	                return (high-low)/2;
+	            }
+	            else
+	                return (high-low)/2+1;
+	}
+	public static void main(String[] args) 
+	{
+	 		System.out.println(countOdds(3,7));
+	}
+			// TODO Auto-generated method stub
+
+		
+}
+
+		
+				
+		        
+	
+
+
