73eb721f394a629c0a5adcec6d0b71fa451a1153
Kaustubh <kaustubh23dom@gmail.com>
2022-07-29 22:53:56 +0530

    <File>"Modification"

--- corejavaprograms/src/com/PrakashBabu/File/File14.java
+++ corejavaprograms/src/com/PrakashBabu/File/File14.java
@@ -6,8 +6,8 @@ public class File14 {
 
 	public static void main(String[] args) throws Exception
 	{
-		FileInputStream fileInputStream= new FileInputStream("C:\\Users\\kaust\\OneDrive\\Documents\\DJ.jpg");
-		FileOutputStream fileOutputStream=new FileOutputStream("PK.jpg");
+		FileInputStream fileInputStream= new FileInputStream("C:\\Users\\kaust\\Downloads\\Profile.jpg");
+		FileOutputStream fileOutputStream=new FileOutputStream("C:\\Users\\kaust\\Downloads\\dp.jpg");
 		byte[] bt=new byte[fileInputStream.available()];	
 		fileInputStream.read(bt);
 		fileOutputStream.write(bt);
