eda81294d79482aa0c5b67615a009ad754807b1b
Kaustubh <kaustubh23dom@gmail.com>
2022-08-16 21:21:49 +0530

    <Database task>"CRUD"

--- corejavaprograms/src/com/kaustubh/database/studentdb/CRUDDemo.java
+++ corejavaprograms/src/com/kaustubh/database/studentdb/CRUDDemo.java
@@ -1,108 +1,164 @@
 package com.kaustubh.database.studentdb;
-
+import java.sql.Statement;
+import java.io.FileInputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class CRUDDemo 
 {
-	public static void main(String[] args)
+	public Connection getConnection() 
+	{
+		Connection connection = null;
+		try 
+		{
+			Properties properties =new Properties();
+			FileInputStream fileInputStream=new FileInputStream("db.prop");
+			properties.load(fileInputStream);
+			String url=properties.getProperty("url");
+			String username =properties.getProperty("username");
+			String  password= properties.getProperty("password");
+			if (connection==null)  
+			{
+				System.out.println("in Connection try .........");
+				Class.forName("org.postgresql.Driver"); 			
+				connection = DriverManager.getConnection(url,username,password); 
+				System.out.println("connection " + connection);
+				System.out.println("Connection to database  Selfbook sucessfully");	
+			} 
+		}	
+		catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		return connection;
+	}
+	public static void main(String[] args) throws SQLException
 	{
 		CRUDDemo crud=new CRUDDemo(); 
-		crud.createTable(); // not needed ideally should be via one time sql scripts
-		crud.insert1Value(); // name should be insert
-		crud.insertAll();
+		Book book=new Book("Rich Dad poor Dad","T101",100,"Robert Kiyosaki");
+		Book book1=new Book("Secret","T104",170,"Rhonda Byrne");
+		Book book2=new Book("Money","T107",205,"BSR");
+		Book book3=new Book("Magic","T100",300,"Rhonda Byrne");
+		crud.insertValue(book); 
+		crud.insertAll(book1, book2, book3);
 		crud.display();
 		crud.delete();
 		crud.deleteAll();
-		
 	}
-	public void createTable()
+	public void insertValue(Book book) throws SQLException
 	{
+
+		CRUDDemo crud=new CRUDDemo(); 
+		Connection connection = null;
 		try
 		{
-     		Connection connect = null; 
-			Statement	 stmt = null;
-			Singelton single=new Singelton();
-			connect=single.getConnection();
-			stmt=connect.createStatement();
-	     	stmt.executeUpdate("CREATE TABLE Book( BookNAME VARCHAR(200),BookID VARCHAR(20),NOOFPAGES INT,AUTHORNAME VARCHAR(50))");
-			stmt.close();	
-			connect.close();
-			System.out.println("Table created sucessfully");
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-			System.exit(0);
-		}
-	}
-	public void insert1Value()	
-	{
-		try
-		{
-			Connection connect = null; 
-			Singelton single=new Singelton();
-			connect=single.getConnection();
-			connect.setAutoCommit(false);
-			String query=" INSERT INTO Book5(Bookname,Bookid,noofpages,authorname)"+" VALUES(?,?,?,?)";
-			PreparedStatement  pstmt= connect.prepareStatement(query);
-			pstmt.setString(1,"Rich Dad poor Dad");
-			pstmt.setInt(2,101);
-			pstmt.setInt(3, 100);
-			pstmt.setString(4,"Robert Kiyosaki");
-			pstmt.addBatch();
-			int[] count=pstmt.executeBatch();
-			 connect.commit();
-			
-			connect.close();
-			System.out.println(count.length+"Records inserted..... into Book5 ");                 
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-			System.exit(0);
-		}
-	}
-	public void insertAll()	
-	{
-		try
-		{
-			Connection connection = null; 
-			Statement stmt = null;
-			Singelton single=new Singelton();
-			connection=single.getConnection();
-			stmt=connection.createStatement();
+
+			connection=crud.getConnection();
 			connection.setAutoCommit(false);
-			String query=" INSERT INTO Book5(Bookname,noofpages,authorname)"+" VALUES(?,?,?,?)";
-			PreparedStatement  pstmt= connection.prepareStatement(query);
-			pstmt.setString(1,"Secret");
-			pstmt.setString(2,"T102"); 
-			pstmt.setInt(2,102);
-			pstmt.setInt(3, 186);
-			pstmt.setString(4,"Rhonda Byrne");
-			pstmt.addBatch();
-			pstmt.setString(1,"Power of subconsicious mind");
-			pstmt.setString(2,"T103");
-			pstmt.setInt(3, 100);
-			pstmt.setString(4,"Joseph murphy");
-			pstmt.addBatch();
-			pstmt.setString(1, "seven habbits");
-			pstmt.setString(2,"T104");
-			pstmt.setInt(3, 100);
-			pstmt.setString(4, "Swapnil");
-			pstmt.addBatch(); 
-			int[] count=pstmt.executeBatch(); 
-			connection.commit();
-			stmt.close(); 
-			connection.close();
-			System.out.println(count.length+"Records inserted..... into Book10");                 
+			String query=" INSERT INTO book2(bookname,bookid,noofpages,authorname)"+" VALUES(?,?,?,?)";
+			PreparedStatement  preparedStatement= connection.prepareStatement(query);
+			preparedStatement.setString(1,book.getBookNAME());
+			preparedStatement.setString(2,book.getBookID()); 
+			preparedStatement.setInt(3,book.getNoofpages());
+			preparedStatement.setString(4,book.getAUTHORNAME());
+			preparedStatement.addBatch();
+			int[] count=preparedStatement.executeBatch();
+			System.out.println(count.length+"Records inserted..... into  ");      
+			System.out.println("Details for  book is........");
+			System.out.println("The book name is "+ book.getBookNAME());	
+			System.out.println("The book id is "+ book.getBookID());	
+			System.out.println("The No of pages of book is "+ book.getNoofpages());	
+			System.out.println("The Author name is "+ book.getAUTHORNAME());	
+			System.out.println("----------------------------------------------------");
 		}
 		catch (Exception e) 
 		{
 			e.printStackTrace();
 			System.exit(0);
+		}
+		finally
+		{
+			if(connection!= null)
+			{
+				connection=crud.getConnection();
+//				connection.commit(); 
+				connection.close();
+			}
+		}
+	}
+	public void insertAll(Book book1,Book book2,Book book3)	 throws SQLException	
+	{
+		Connection connection = null; 
+		Statement statement = null;
+		CRUDDemo crud=new CRUDDemo(); 
+		try
+		{
+			connection=crud.getConnection();
+			statement=connection.createStatement();
+			//				connection.setAutoCommit(false);
+			String query=" INSERT INTO Book2(Bookname,Bookid,noofpages,authorname)"+" VALUES(?,?,?,?)";
+			PreparedStatement  preparedStatement= connection.prepareStatement(query);
+			preparedStatement.setString(1,book1.getBookNAME());
+			preparedStatement.setString(2,book1.getBookID()); 
+			preparedStatement.setInt(3,book1.getNoofpages());
+			preparedStatement.setString(4,book1.getAUTHORNAME());
+			preparedStatement.addBatch();
+			System.out.println("2nd record inserted sucessfully");
+//			System.out.println("Details for  book is........");
+//			System.out.println("The book name is "+ book1.getBookNAME());	
+//			System.out.println("The book id is "+ book1.getBookID());	
+//			System.out.println("The No of pages  is "+ book1.getNoofpages());	
+//			System.out.println("The Author name is "+ book1.getAUTHORNAME());	
+//			System.out.println("----------------------------------------------------");
+//			preparedStatement.setString(1,book2.getBookNAME());
+			preparedStatement.setString(2,book2.getBookID()); 
+			preparedStatement.setInt(3,book2.getNoofpages());
+			preparedStatement.setString(4,book2.getAUTHORNAME());
+			preparedStatement.addBatch();
+	
+			System.out.println("3rd record inserted sucessfully");
+//			System.out.println("Details for  book is........");
+//			System.out.println("The book name is "+ book2.getBookNAME());	
+//			System.out.println("The book id is "+ book2.getBookID());	
+//			System.out.println("The No of pages  is "+ book2.getNoofpages());	
+//			System.out.println("The Author name is "+ book2.getAUTHORNAME());	
+//			System.out.println("----------------------------------------------------");
+			
+			preparedStatement.setString(1,book3.getBookNAME());
+			preparedStatement.setString(2,book3.getBookID()); 
+			preparedStatement.setInt(3,book3.getNoofpages());
+			preparedStatement.setString(4,book3.getAUTHORNAME());
+			preparedStatement.addBatch();
+			preparedStatement.executeBatch(); 
+			System.out.println("4th record inserted sucessfully");
+//			System.out.println("Details for  book is........");
+//			System.out.println("The book name is "+ book3.getBookNAME());	
+//			System.out.println("The book id is "+ book3.getBookID());	
+//			System.out.println("The No of pages  is "+ book3.getNoofpages());	
+//			System.out.println("The Author name is "+ book3.getAUTHORNAME());	
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+		finally
+		{
+			if(statement!= null)
+			{
+				statement.close(); 
+			}
+			if(connection!= null)
+			{
+				connection=crud.getConnection();
+				//					connection.commit();
+				connection.close();
+			}
 		}
 	}
 	public void display() 
@@ -113,7 +169,7 @@ public class CRUDDemo
 			Connection connect = null; 
 			Singelton single=new Singelton();
 			connect=single.getConnection();
-			String query="Select*from Book10";
+			String query="Select*from Book2";
 			PreparedStatement  preparedStatement= connect.prepareStatement(query);
 			resultSet = preparedStatement.executeQuery();
 		    while(resultSet.next())
@@ -122,7 +178,6 @@ public class CRUDDemo
 		    	System.out.println(resultSet.getString(2));
 		    	System.out.println(resultSet.getInt(3));	
 		    	System.out.println(resultSet.getString(4));
-		    	
 		    	System.out.println(" ");
 		    }
 		}
@@ -136,14 +191,14 @@ public class CRUDDemo
 	{
 		try
 		{
-			Connection connect = null; 
-			Statement stmt = null;
+			Connection connection = null; 
+			Statement statement = null;
 			Singelton single=new Singelton();
-			connect=single.getConnection();
-			stmt=connect.createStatement();
-			stmt.executeUpdate("delete from Book6 where BookNAME='Secret'");
-			stmt.close();	
-			connect.close();
+			connection=single.getConnection();
+			statement =connection.createStatement();
+			statement .executeUpdate("delete from Book2 where BookNAME='Secret'");
+			statement .close();	
+			connection.close();
 			System.out.println("Record Deleted");                 
 		}
 		catch (Exception e) 
@@ -156,14 +211,14 @@ public class CRUDDemo
 	{
 		try
 		{
-			Connection connect = null; 
-			Statement stmt = null;
+			Connection connection = null; 
+			Statement statement = null;
 			Singelton single=new Singelton();
-			connect=single.getConnection();
-			stmt=connect.createStatement();
-			stmt.executeUpdate("DELETE FROM Book6");
-			stmt.close();	
-			connect.close();
+			connection=single.getConnection();
+			statement =connection.createStatement();
+			statement.executeUpdate("DELETE FROM Book2");
+			statement.close();	
+			connection.close();
 			System.out.println("All Record Deleted");                 
 		}
 		catch (Exception e) 
