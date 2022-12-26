7de25372a3dd7fdad909b452ce871cd2b0287700
Kaustubh <kaustubh23dom@gmail.com>
2022-12-26 10:12:33 +0530

    <Task2>"Largest and smallest of sentence"

---
+++ com.kaustubh.string/src/com/kaustubh/string/LargestString.java
@@ -0,0 +1,33 @@
+package com.kaustubh.string;
+
+public class LargestString 
+{
+	
+	public static void maximumminimum(String string)
+	{
+		String[] words=string.split(" ");
+		String smallestWord=" ";
+		String largestWord=" ";
+		int minimumLength=Integer.MAX_VALUE;
+		int maximumLength=Integer.MIN_VALUE;
+		for(int i=0;i<words.length;i++)
+		{
+			if(words[i].length()>maximumLength)
+			{
+				maximumLength=words[i].length();
+				largestWord=words[i];
+			}
+			if(words[i].length()<minimumLength)
+			{
+				minimumLength=words[i].length();
+				smallestWord=words[i];
+			}	
+		}
+		System.out.println("Largest word is "+largestWord+" and smallest word is "+smallestWord);
+		
+	}
+	public static void main(String[] args) 
+	{
+				LargestString.maximumminimum("I Belong To India");
+	}
+}
